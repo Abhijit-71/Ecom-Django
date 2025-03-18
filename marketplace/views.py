96317c837729ead6281c5c4b0f62d3cb96f77cbc
Abhijit71 <4c.abhijit71@gmail.com>
2025-03-18 20:39:17 +0530

    created reg. page

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#creating views for home page route
+def home(request):
+    return render(request, '')  
+
+#views for marketplace page
+def marketplace(request):
+    return render(request,'')   #html template address in '' as arguement to render method
